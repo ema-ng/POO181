4339ef2c2228f2ac5e92484ae08ab5ed70ecde2f
ema-ng <125697790+ema-ng@users.noreply.github.com>
2023-02-20 18:43:16 -0600

    definimos metodos del personaje

--- personaje.py
+++ personaje.py
@@ -1,5 +1,22 @@
 class personaje:
     
+    #atributos de clase 
     especie="humano"
     nombre="marcus fenix"
     altura= 1.90
+    
+    #metodos de personaje 
+    
+    def correr (self,status):
+        if(status):
+            print("el personaje" + self.nombre + "esta corriendo")
+        else:
+            print("el personaje " + self.nombre +"se detubo")
+    
+    def lanzargranada (self):
+        print("se lanzo granada")
+        
+    def recargararma (self, municiones):
+        cargador = 5
+        cargador = cargador + municiones
+        print("el arma tiene ahora" + cargador + "balas")
