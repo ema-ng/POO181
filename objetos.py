b36ff2d69255949fe5af08430c139314256e0b53
ema-ng <125697790+ema-ng@users.noreply.github.com>
2023-02-24 18:25:35 -0600

    ejemplo encapsulamiento de atributos

--- objetos.py
+++ objetos.py
@@ -7,41 +7,48 @@ from personaje import *
  
 print("")
 print("###solicitud de datos del heroe ###")
-esph=input("escribe la especie del heroe")
-nomh=input("escribe el nombre del heroe")
-alth=float(input("escribe la altura del heroe"))
-balash=int(input("cuantas balas se recargaran al heroe"))
+esph=input("escribe la especie del heroe: ")
+nomh=input("escribe el nombre del heroe: ")
+alth=float(input("escribe la altura del heroe: "))
+balash=int(input("cuantas balas se recargaran al heroe: "))
 
 print("")
 print("###solicitud de datos del villano ###")
-espv=input("escribe la especie del villano")
-nomv=input("escribe el nombre del villano")
-altv=float(input("escribe la altura del villano"))
-balasv=int(input("cuantas balas se recargaran al villano"))
+espv=input("escribe la especie del villano: ")
+nomv=input("escribe el nombre del villano: ")
+altv=float(input("escribe la altura del villano: "))
+balasv=int(input("cuantas balas se recargaran al villano: "))
 
 #3.creamos 2 objetos
 
 heroe = personaje(esph,nomh,alth)
 villano = personaje(espv,nomv,altv)
 
+#ejemplo del set
+
+heroe.__setnombre__("pepe pecas")
+
 #4. acceder a sus atributos y metodos decada obj
 
 print("")
 print("## atributos y metodos del heroe ## ")
-print("el personaje pertenece a la raza: " + heroe.especie)
-print("se llama: " + heroe.nombre)
-print("mide: " + str(heroe.altura) + " metros")
+print("el personaje pertenece a la raza: " + heroe.__getespecie__())
+print("se llama: " + heroe.__getnombre__())
+print("mide: " + str(heroe.__getaltura__()) + " metros")
 print("")
 
 heroe.correr(True)
 heroe.lanzargranada
 heroe.recargararma(balash)
 
+#ejemplo de lo que no hay que hacer
+#heroe.__pensar()
+
 print("")
 print("## atributos y metodos del heroe ## ")
-print("el personaje pertenece a la raza: " + villano.especie)
-print("se llama: " + villano.nombre)
-print("mide: " + str(villano.altura) + " metros")
+print("el personaje pertenece a la raza: " + villano.__getespecie__())
+print("se llama: " + villano.__getnombre__())
+print("mide: " + str(villano.__getaltura__()) + " metros")
 print("")
 
 villano.correr(False)
--- personaje.py
+++ personaje.py
@@ -4,17 +4,17 @@ class personaje:
     
     def __init__(self, esp,nom,alt):
         
-        self.especie= esp
-        self.nombre= nom
-        self.altura= alt
+        self.__especie= esp
+        self.__nombre= nom
+        self.__altura= alt
     
     #metodos de personaje 
     
     def correr (self,status):
         if(status):
-            print("el personaje " + self.nombre + " esta corriendo")
+            print("el personaje " + self.__nombre + " esta corriendo")
         else:
-            print("el personaje " + self.nombre +" se detubo")
+            print("el personaje " + self.__nombre +" se detubo")
     
     def lanzargranada (self):
         print("se lanzo granada")
@@ -23,3 +23,28 @@ class personaje:
         cargador = 5
         cargador = cargador + municiones
         print("el arma tiene ahora " + str(cargador) + " balas")
+    
+    """ 
+    def __pensar (self):
+        print("estoy pensando")
+     """
+     
+    # declaramos los getters y setters de los atributos priveados 
+        
+    def __getespecie__(self):
+        return self.__especie
+    
+    def __setespecie__(self,esp):
+        self.__especie=esp
+    
+    def __getnombre__(self):
+        return self.__nombre
+    
+    def __setnombre__(self,nom):
+        self.__nombre=nom
+        
+    def __getaltura__(self):
+        return self.__altura
+    
+    def __setaltura__(self,alt):
+        self.__altura=alt
