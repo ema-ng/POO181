f3ee08a3e7672fa4e252148ba9f7389c4d488377
ema-ng <125697790+ema-ng@users.noreply.github.com>
2023-02-27 18:54:54 -0600

    interfaces tk,frame,button

--- tkinther/practica11.py
+++ tkinther/practica11.py
@@ -21,32 +21,18 @@ seccion3 = Frame(ventana,bg="yellow")
 seccion3.pack(expand=True,fill='both')
 
 
-
-seccion1 = Frame(ventana, bg="green")
-seccion1.pack(expand=True,fill='both')
-
-seccion1 = Frame(ventana, bg="blue")
-seccion1.pack(expand=True,fill='both')
-
-seccion1 = Frame(ventana, bg="purple")
-seccion1.pack(expand=True,fill='both')
-
-
 #3.botones 
 
-"""
 botonAzul= Button(seccion1, text="boton azul", fg="blue")
 botonAzul.place(x=60, y=60)
 
 botonblanco= Button(seccion2, text="boton blanco", fg="pink")
 botonblanco.grid(row=0,column=0)
 
-botonnegro= Button(seccion2, text="boton negro", fg="black")
+botonnegro= Button(seccion2, text="boton negro", fg="black", bg="white")
 botonnegro.grid(row=1,column=1)
 
-"""
-
-botonverde= Button(seccion3, text="leonardo daniel", fg="green")
+botonverde= Button(seccion3, text="boton verde", fg="green")
 botonverde.pack()
 
 
