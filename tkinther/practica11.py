0e6bfb0ab72c942187d9de9cdfad67294530dff2
ema-ng <125697790+ema-ng@users.noreply.github.com>
2023-03-01 18:01:50 -0600

    crear mensajes

--- tkinther/practica11.py
+++ tkinther/practica11.py
@@ -1,5 +1,21 @@
 
-from tkinter import Tk, Frame, Button
+from tkinter import Tk, Frame, Button,messagebox
+
+def mostrarMensaje():
+    messagebox.showinfo("aviso","este mensaje es para avisar algo")
+    messagebox.showerror("error","todo fallo con exito")
+    print(messagebox.askokcancel("pregunta","el o ella jugo con tu corazon"))
+    print(messagebox.askquestion("pregunta","el o ella jugo con tu corazon"))
+    print(messagebox.askretrycancel("pregunta","el o ella jugo con tu corazon"))
+    print(messagebox.askyesno("pregunta","el o ella jugo con tu corazon"))
+    print(messagebox.askyesnocancel("pregunta","el o ella jugo con tu corazon"))
+    
+#funcion para agregar botones 
+
+def agregarbotones():
+    botonverde.config(text="+",bg="green",fg="white")
+    botonnuevo=Button(seccion3,text="boton nuevo")
+    botonnuevo.pack()
 
 #1.instanciamon un objeto ventana 
 
@@ -13,26 +29,26 @@ seccion1 = Frame(ventana, bg="red")
 seccion1.pack(expand=True,fill='both')
 
 
-seccion2 = Frame(ventana, bg="orange")
+seccion2 = Frame(ventana, bg="blue")
 seccion2.pack(expand=True,fill='both')
 
 
-seccion3 = Frame(ventana,bg="yellow")
+seccion3 = Frame(ventana,bg="green")
 seccion3.pack(expand=True,fill='both')
 
 
 #3.botones 
 
-botonAzul= Button(seccion1, text="boton azul", fg="blue")
+botonAzul= Button(seccion1, text="boton azul", fg="blue",command=mostrarMensaje)
 botonAzul.place(x=60, y=60)
 
-botonblanco= Button(seccion2, text="boton blanco", fg="pink")
+botonblanco= Button(seccion2, text="boton blanco", fg="pink",bg="#ffff1a")
 botonblanco.grid(row=0,column=0)
 
-botonnegro= Button(seccion2, text="boton negro", fg="black", bg="white")
+botonnegro= Button(seccion2, text="boton negro", fg="white", bg="black")
 botonnegro.grid(row=1,column=1)
 
-botonverde= Button(seccion3, text="boton verde", fg="green")
+botonverde= Button(seccion3, text="boton verde", fg="green",command=agregarbotones)
 botonverde.pack()
 
 
